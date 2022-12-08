11701d5910106ccb530e3b6ecef55b167da04d12
Mohab <Mohab@DESKTOP-0BJ6OR8>
2022-12-08 15:33:54 +0200

    Created class customer without the methods that require GUI

---
+++ supermarketSystem/customer.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace supermarketSystem
+{
+    class customer
+    {
+        string fullName, password, email, phoneNumber, address, id;
+        double cashBalance;
+        List<Tuple<string, int>> cart = new List<Tuple<string, int>>(); // product id and quantity
+        List<string> purchaseLog = new List<string>(); // all invoices for this customer
+
+        public customer(string fullName, string password, string email,
+            string phoneNumber, string address, string id, double cashBalance)
+        {
+            this.fullName = fullName;
+            this.password = password;
+            this.email = email;
+            this.phoneNumber = phoneNumber;
+            this.address = address;
+            this.id = id;
+            this.cashBalance = cashBalance;
+
+            string path = "CustomerID_" + Id;
+            Program.writeOnFile(path, fullName);
+            Program.writeOnFile(path, password);
+            Program.writeOnFile(path, email);
+            Program.writeOnFile(path, phoneNumber);
+            Program.writeOnFile(path, address);
+            Program.writeOnFile(path, id);
+            Program.writeOnFile(path, cashBalance.ToString());
+        }
+
+        void updateFile(int idx, string val)
+        {
+            string path = "CustomerID_" + Id;
+            List<string> data = Program.readFromFile(path);
+            data[idx] = val;
+            foreach (var item in data)
+                Program.writeOnFile(path, item);
+        }
+
+        public string FullName
+        {
+            get { return fullName; }
+            set
+            {
+                updateFile(0, value);
+                fullName = value;
+            }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                updateFile(1, value);
+                password = value;
+            }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                updateFile(2, value);
+                email = value;
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set
+            {
+                updateFile(3, value);
+                phoneNumber = value;
+            }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set
+            {
+                updateFile(4, value);
+                address = value;
+            }
+        }
+
+        public string Id
+        {
+            get { return id; }
+            set
+            {
+                updateFile(5, value);
+                id = value;
+            }
+        }
+
+        public double CashBalance
+        {
+            get { return cashBalance; }
+            set
+            {
+                updateFile(6, value.ToString());
+                cashBalance = value;
+            }
+        }
+
+        void addToCart(string productId)
+        {
+            // form to take the quantity of that product
+        }
+
+        void proceedToCheckout()
+        {
+            // Pay form
+        }
+
+        void customerMainMenu()
+        {
+            // User main menu form
+        }
+    }
+}
