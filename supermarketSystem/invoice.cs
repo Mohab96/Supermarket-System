8157e81d74c1ab3fb2fdf22dc4e56f29944ce16c
mostafa <mostafaamen305@gmail.com>
2022-12-10 11:08:11 +0200

    complete invoice class and product

--- supermarketSystem/invoice.cs
+++ supermarketSystem/invoice.cs
@@ -1,23 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+
+
 
 namespace supermarketSystem
 {
     public class invoice
     {
         private DateTime date;
+        List<product> productlist ;  
+        // List<int,product> productlist
         private int tax;
         private string id;
         private int discountPer;
         private double totalprice;
 
+        public invoice(DateTime date , int tax , string id , int discountPer , double totalprice)
+        {
+            this.date = date;
+            this.productlist = new List<product>() ;
+            this.tax = tax;
+            this.id= id;
+            this.discountPer = discountPer ;
+            this.totalprice = totalprice;
+
+            string path = "invoice id : " + id + ".txt";
+            Global.writeOnFile(path, String.Empty);
+            Global.writeOnFile(path, date.ToString());
+            Global.writeOnFile(path, tax.ToString());
+            Global.writeOnFile(path, id.ToString());
+            Global.writeOnFile(path, discountPer.ToString());
+            Global.writeOnFile(path, totalprice.ToString());
+
+            foreach (product p in productlist)
+            { Global.writeOnFile(path, p.ToString() ); }
+
+        }
         /// (mohab) => (mas) : Add the constructor and format the text file in the same constructor
-
         /// (mohab) => (mas) : Add the products list
-
         /// (mohab) => (mas) : Add the method that download the invoice as pdf file
+
+        public void DownloadFile(Uri address, string fileName)
+        {
+
+        }
+        
+
+        ///(mas) => (team) : 
+        ///1- how can i init. list with two Parameter..note:C# doesn't have built-in Map 
+
+        /// 2- download file  : with my search i found this way but i dont understand it totaly
     }
 }
--- supermarketSystem/product.cs
+++ supermarketSystem/product.cs
@@ -12,36 +12,110 @@ namespace supermarketSystem
 {
     public class product
     {
-        private string name;
+        private string productname;
         private int quantity;
         private double price;
-        private double discount;
+        private int discount;
         private DateTime expirydate;
-        private PictureBox image;
-        string imgUrl; /// (mohab) => (mas) : need this to be able to load the image when the databse loads
+        /// private PictureBox image;
+        string imgUrl; /// (mas) => (mohab) : i dont understand this var
 
         /// (mohab) => (mas) : Finish the class with files and add the body of the setters 
+        /// (mas) => (mohab) : خلصانه
 
-        public product(string name, int quantity, double price, double discount, DateTime expirydate, string imgUrl)
+        public product(string productname, int quantity, double price, int  discount, DateTime expirydate, string imgUrl)
         {
             /// (mohab) => (mas) : I removed picturebox from the parameters and added the url of the image to be able to load it form the files
-            this.name = name;
+            /// (mas) => (mohab) : هعديهالك المرادي
+            this.productname = productname;
             this.quantity = quantity;
             this.price = price;
             this.discount = discount;
             this.expirydate = expirydate;
             this.imgUrl = imgUrl;
 
-            /// (mohab) => (mas) : add update the files here
+            string path = "productname" + productname + ".txt";
+
+            Global.writeOnFile(path, String.Empty);
+            Global.writeOnFile(path, productname);
+            Global.writeOnFile(path, quantity.ToString());
+            Global.writeOnFile(path, price.ToString());
+            Global.writeOnFile(path, discount.ToString());
+            Global.writeOnFile(path, expirydate.ToString());
+            Global.writeOnFile(path, imgUrl.ToString());
+            
+
+
+            /// (mas) => (mohab) : done ya 3tlaan
+        }
+
+        /// (mas) => (mohab) : i copied this fun "updateFile" from custmer class and make an edit on my copy
+        /// because there are an erorr with updatefile  "updatefile dont declared"
+        void updateFile(int idx, string val)
+        {
+            string path = "CustomerID_" + productname + ".txt";
+            List<string> productdata = Global.readFromFile(path);
+            productdata[idx] = val;
+            Global.writeOnFile(path, String.Empty); // Clear the file before writing on it
+            foreach (var item in productdata)
+                Global.writeOnFile(path, item);
         }
 
         /// (mohab) => (mas) : add the body of the setters
-        public string Name { get; set; }
-        public int Quantity { get; set; }
-        public int Price { get; set; }
-        public int Discount { get; set; }
-        public int Expirydate { get; set; }
-        public int Image { get; set; }
+        /// (mas) => (mohab) : ما قولنا خلصانه هو حوار 
+        /// راجع بس كده على شغل الفايلز بتاعي 
+        public string Name {
+            get { return productname; }
+            set
+            {
+                updateFile(0, value);
+                productname = value;
+            }
+        }
+        public int Quantity {
+            get { return quantity; }
+            set
+            {
+                updateFile(1, value.ToString());
+                quantity = value;
+            }
+        }
+        public double Price {
+            get { return price; }
+            set
+            {
+                updateFile(2, value.ToString());
+                price = value;
+            }
+        }
+        public int Discount
+        {
+            get { return discount; }
+            set
+            {
+                updateFile(3, value.ToString());
+                discount = value;
+            }
+        }
+        public DateTime Expirydate {
+            get { return expirydate; }
+            set
+            {
+                updateFile(4, value.ToString());
+                expirydate = value;
+            }
+        }
+
+        /// (mas) => (mohab) : 
+        public string Image
+        {
+            get { return imgUrl; }
+            set
+            {
+                updateFile(5, value.ToString());
+                imgUrl = value;
+            }
+        }
 
     }
 }
