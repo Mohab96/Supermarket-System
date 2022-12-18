13e37db4371fdeb1dedc32575c04c296a128c108
arwa <arwaibrahimsaad2003@gmail.com>
2022-12-18 12:58:18 +0200

    added form3

---
+++ supermarketSystem/Form3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace supermarketSystem
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/Mohab96/Supermarket-System");
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            textBox2.Text = DateTime.Now.ToString("dd/mm/yyy");
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
